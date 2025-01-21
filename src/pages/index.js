2835574b0009178c037ef211dae674e38b788c70
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-21 15:37:22 +0330

    fix font URL in global styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <h1 className="text-4xl font-yekan text-center mt-10">سلام جهان !</h1>
+    </>
        
   );
 }
