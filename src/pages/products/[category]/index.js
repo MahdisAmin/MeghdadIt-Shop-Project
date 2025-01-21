ef09dff560a3a92a55769fd177afbbdb18f825c8
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-21 19:37:03 +0330

    add ProductsByCategories component to display products by category

---
+++ src/pages/products/[category]/index.js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+function ProductsByCategories() {
+  const { category } = useRouter().query;
+
+  return <div>ProductsByCategories - {category}</div>;
+}
+
+export default ProductsByCategories;
