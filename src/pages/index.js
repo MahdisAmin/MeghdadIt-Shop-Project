c3393ff56dac417cf5f9843fdf949e4c6f91efc4
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-21 18:48:47 +0330

    refactor: remove _document.js, add the routes About, Article, and Contact pages, and update index.js and globals.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-4xl font-yekan text-center mt-10">سلام جهان !</h1>
+      <h1 className="">سلام جهان !</h1>
     </>
        
   );
