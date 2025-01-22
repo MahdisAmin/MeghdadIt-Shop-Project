513aa4d6afd2ce723da46d493b5db26a53b1124a
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-22 17:11:24 +0330

    add menu icon to Navbar component and enhance link styles for better visibility

--- src/components/elements/Navbar.js
+++ src/components/elements/Navbar.js
@@ -1,22 +1,40 @@
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Navbar() {
   return (
     <div className="bg-white border-b border-b-gray-100">
       <div className="container mx-auto">
         <ul className="flex items-center p-5">
-          <li className="px-4">
-            <Link href="/">صفحه اصلی</Link>
+          <li className="md:hidden">
+            <Image
+              src="/images/svgs/menu.svg"
+              width={20}
+              height={20}
+              alt="menu"
+              className=" cursor-pointer"
+            ></Image>
           </li>
           <li className="px-4">
-            <Link href="/article">مقالات</Link>
+            <Link href="/" className="text-gray-800 hover:text-blue-600 ">
+              صفحه اصلی
+            </Link>
           </li>
           <li className="px-4">
-            <Link href="/about">درباره ما</Link>
+            <Link href="/article" className="text-gray-800 hover:text-blue-600">
+              مقالات
+            </Link>
           </li>
           <li className="px-4">
-            <Link href="/contact">تماس با ما</Link>
+            <Link href="/about" className="text-gray-800 hover:text-blue-600">
+              درباره ما
+            </Link>
+          </li>
+          <li className="px-4">
+            <Link href="/contact" className="text-gray-800 hover:text-blue-600">
+              تماس با ما
+            </Link>
           </li>
         </ul>
       </div>
@@ -24,4 +42,4 @@ function Navbar() {
   );
 }
 
-export default Navbar
+export default Navbar;
