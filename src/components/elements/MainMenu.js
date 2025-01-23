46048bdaafec05519cba8103523b48efd40d3f92
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-23 16:08:55 +0330

    add MainMenu component with product links and integrate it into Header; update package.json and package-lock.json to include react-icons

--- src/components/elements/MainMenu.js
+++ src/components/elements/MainMenu.js
@@ -1,9 +1,59 @@
-import React from 'react'
-
+import Link from "next/link";
+import { RxCaretDown } from "react-icons/rx";
 function MainMenu() {
   return (
-    <div>MainMenu</div>
-  )
+    <div className="hidden bg-white border-b border-b-gray-100 md:block">
+      <div className="container mx-auto">
+        <ul className="flex items-center p-5">
+          <li className="flex items-center px-4">
+            <Link
+              href="/products/laptops"
+              className="text-gray-800 text-sm hover:text-gray-950 hover:font-bold"
+            >
+              لپتاپ
+            </Link>
+            <RxCaretDown />
+          </li>
+          <li className="flex items-center  px-4">
+            <Link
+              href="/products/mobiles"
+              className="text-gray-800 text-sm hover:text-gray-950 hover:font-bold"
+            >
+              موبایل{" "}
+            </Link>
+            <RxCaretDown />
+          </li>
+          <li className="flex items-center  px-4">
+            <Link
+              href="/products/tablets"
+              className="text-gray-800 text-sm hover:text-gray-950 hover:font-bold"
+            >
+              تبلت
+            </Link>
+            <RxCaretDown />
+          </li>
+          <li className="flex items-center  px-4">
+            <Link
+              href="/products/cameras"
+              className="text-gray-800 text-sm hover:text-gray-950 hover:font-bold"
+            >
+              دوربین
+            </Link>
+            <RxCaretDown />
+          </li>
+          <li className="flex items-center  px-4">
+            <Link
+              href="/products/games"
+              className="text-gray-800 text-sm hover:text-gray-950 hover:font-bold"
+            >
+              کنسول و بازی
+            </Link>
+            <RxCaretDown />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default MainMenu
+export default MainMenu;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../elements/Navbar'
+import MainMenu from '../elements/MainMenu'
 
 function Header() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <MainMenu/>
     </>
   )
 }
