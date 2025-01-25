7ed5ffb8c46e4934d4455fef2505fde9184c902b
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-25 17:31:05 +0330

    refactor ProductsCards component layout for improved structure and responsiveness; adjust price display positioning

--- src/components/elements/ProductsCards.js
+++ src/components/elements/ProductsCards.js
@@ -3,8 +3,8 @@ import React from "react";
 function ProductsCards({ image , text , price }) {
 
   return (
-    <div className="bg-white shadow-md hover:transition-all hover:shadow-gray-400 shadow-gray-200 rounded-xl overflow-hidden">
-      <div className="h-[350px] flex justify-center items-center">
+    <div className="h-[350px] flex flex-col justify-between bg-white shadow-md hover:transition-all hover:shadow-gray-400 shadow-gray-200 rounded-xl overflow-hidden">
+      <div className=" h-[200ox] flex justify-center items-center">
         <img src={image} alt={text} className=" object-contain" />
       </div>
       <div>
@@ -13,8 +13,8 @@ function ProductsCards({ image , text , price }) {
         </p>
       </div>
       <div className="border-t border-t-gray-400 flex justify-between items-center py-[15px] px-[20px]">
-        <p className="text-sm text-gray-800">{price}</p>
         <button className="bg-orange-400 rounded-xl p-1 text-sm cursor-pointer hover:bg-orange-500">افزودن به سبد خرید</button>
+        <p className="text-sm text-gray-800 p-3">{price}</p>
       </div>
     </div>
   );
