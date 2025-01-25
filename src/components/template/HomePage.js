c93c34181d3f7bd8c468029473dd85899e3f8f7a
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-25 17:30:52 +0330

    add camera section to HomePage; update layout for laptops and mobiles for improved organization and user experience

--- src/components/template/HomePage.js
+++ src/components/template/HomePage.js
@@ -2,18 +2,47 @@ import data from "../../../data/db.json";
 import ProductsCards from "../elements/ProductsCards";
 
 function HomePage() {
-  const { laptops, mobiles, tablets } = data;
+  const { laptops, mobiles, cameras } = data;
 
   return (
-    <div className="container text-center my-12 mx-auto">
-      <div className="flex flex-wrap m-4">
-        {laptops.slice(0,4).map((laptop) => (
-          <div className="p-4 w-full md:w-1/3 lg:w-1/4" key={laptop.id}>
-                <ProductsCards {...laptop} />
-          </div>
-        ))}
+    <>
+      <div className="container m-12 mx-auto">
+        <h1 className="font-bold px-8">لپ تاپ (laptop) </h1>
       </div>
-    </div>
+      <div className="container text-center my-12 mx-auto">
+        <div className="flex flex-wrap m-4">
+          {laptops.slice(0, 4).map((laptop) => (
+            <div className="p-4 w-full md:w-1/3 lg:w-1/4" key={laptop.id}>
+              <ProductsCards {...laptop} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="container m-12 mx-auto">
+        <h1 className="font-bold px-8">موبایل(mobile) </h1>
+      </div>
+      <div className="container text-center my-12 mx-auto">
+        <div className="flex flex-wrap m-4">
+          {mobiles.slice(0, 4).map((mobiles) => (
+            <div className="p-4 w-full md:w-1/3 lg:w-1/4" key={mobiles.id}>
+              <ProductsCards {...mobiles} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="container m-12 mx-auto">
+        <h1 className="font-bold px-8">موبایل(mobile) </h1>
+      </div>
+      <div className="container text-center my-12 mx-auto">
+        <div className="flex flex-wrap m-4">
+          {cameras.slice(0, 4).map((camera) => (
+            <div className="p-4 w-full md:w-1/3 lg:w-1/4" key={camera.id}>
+              <ProductsCards {...camera} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
