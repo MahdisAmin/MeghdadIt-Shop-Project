76bbb6ac40a42bce2c934eefa453e00841fad5a9
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-27 01:36:39 +0330

    add search functionality to ProductsByCategories component; implement filtering based on search input

--- src/pages/products/[category]/index.js
+++ src/pages/products/[category]/index.js
@@ -6,14 +6,22 @@ import { useEffect, useState } from "react";
 function ProductsByCategories() {
   const { category } = useRouter().query;
   const [datas, setDatas] = useState(data[category]);
- 
+  const [searchKey , setSearchKey] = useState(null);
+  
   useEffect(() => { 
     setDatas(data[category]);
   }, category);
+
+  useEffect(() => { 
+    if(searchKey === null) return setDatas(datas);
+     setDatas(data[category].filter((item) => item.text.includes(searchKey)))
+  },[searchKey])
+
   return (
     <div>
       <div className="container m-12 mx-auto text-center">
         <input
+          onChange={(e) => setSearchKey(e.target.value)}
           type="text"
           placeholder="دنبال چی میگردی؟"
           className="p-1 outline-none rounded-md w-96 border border-gray-400"
