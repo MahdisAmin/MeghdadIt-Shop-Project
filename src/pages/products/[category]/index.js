ba0981a31766786c6aeafd0f6b1d873d4852441c
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-26 23:03:02 +0330

    add ProductsByCategories component to display products by category; integrate ProductsCards for item rendering

--- src/pages/products/[category]/index.js
+++ src/pages/products/[category]/index.js
@@ -1,10 +1,29 @@
 import { useRouter } from "next/router";
-import React from "react";
+import data from "../../../../data/db.json";
+import ProductsCards from "@/components/elements/ProductsCards";
 
 function ProductsByCategories() {
+  // console.log(data);
+  
   const {category}= useRouter().query
+  
+  const datas = data[category] 
+  console.log(datas)
 
-    return <div>ProductsByCategories - {category}</div>;
+  return (
+    <div>
+      <h4 className="text-center my-5 font-bold text-xl">{category}</h4>
+      <div className="container text-center my-12 mx-auto">
+        <div className="flex flex-wrap m-4">
+          {datas?.map((item) => (
+            <div className="p-4 w-full md:w-1/3 lg:w-1/4" key={item.id}>
+              <ProductsCards {...item} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductsByCategories;
