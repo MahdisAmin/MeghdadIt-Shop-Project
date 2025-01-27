94474d09d0cd448a48733945d023641c1e126dc8
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-28 01:27:46 +0330

    update App component; reset menu visibility on route change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,20 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
-import { createContext, useState } from "react";
+import { useRouter } from "next/router";
+import { createContext, useEffect, useState } from "react";
 
 export const AppContext = createContext();
 
 export default function App({ Component, pageProps }) {
   const [showMenu, setShowMenu] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setShowMenu(false);
+  }, [router.asPath]);
 
   return (
-    <AppContext.Provider value={{setShowMenu , showMenu}}>
+    <AppContext.Provider value={{ setShowMenu, showMenu }}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
