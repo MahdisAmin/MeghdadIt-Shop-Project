73ae63919e29bde74a29da2f0517e2e168c4b0cb
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-28 01:14:44 +0330

    update App component; implement context for managing menu visibility

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,17 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
+import { createContext, useState } from "react";
+
+export const AppContext = createContext();
 
 export default function App({ Component, pageProps }) {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    
-    </Layout>
+    <AppContext.Provider value={{setShowMenu , showMenu}}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppContext.Provider>
   );
 }
