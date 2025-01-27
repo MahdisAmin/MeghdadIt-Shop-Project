90867ce3f46d88eac9bc62a6af3be63e26e3f0b7
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-27 22:06:35 +0330

    update ProductsByBrands component; implement data filtering by brand and enhance layout for better presentation

--- src/pages/products/[category]/[brand].js
+++ src/pages/products/[category]/[brand].js
@@ -1,10 +1,28 @@
 import { useRouter } from "next/router";
-
+import data from "../../../../data/db.json";
+import { useEffect, useState } from "react";
+import ProductsCards from "@/components/elements/ProductsCards";
 function ProductsByBrands() {
   const { category, brand } = useRouter().query;
+  const [datas, setDatas] = useState(data[category]);
+
+  useEffect(() => {
+    const filteredData = data[category]?.filter((item) => item.brand === brand);
+    setDatas(filteredData);
+  }, [category, brand]);
+
   return (
     <div>
-      ProductsByBrands - {category} - {brand}
+       <h1 className="container mx-auto font-bold text-lg p-2 m-4 text-center">لپتاپ-{(brand)}</h1>
+      <div className="container text-center my-12 mx-auto">
+        <div className="flex flex-wrap m-4">
+          {datas?.map((item) => (
+            <div className="p-4 w-full md:w-1/3 lg:w-1/4" key={item.id}>
+              <ProductsCards {...item} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
