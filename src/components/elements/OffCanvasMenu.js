0e701c2e62f12020e12ea7b6cbdef4e6d7cae202
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-27 23:14:14 +0330

    update OffCanvasMenu component; implement sidebar menu with product links and enhance layout for better user experience

--- src/components/elements/OffCanvasMenu.js
+++ src/components/elements/OffCanvasMenu.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import Link from "next/link";
+import { MdOutlineClose } from "react-icons/md";
+import { RxCaretDown } from "react-icons/rx";
 
 function OffCanvasMenu() {
   return (
-    <div>OffCanvasMenu</div>
-  )
+    <aside className="h-[100vh] w-[280px] bg-white fixed top-0 right-0 z-50 shadow-lg overflow-hidden transition-all duration-[0.5s] ease-in-out">
+      <div className="flex justify-between items-center p-4 bg-blue-500 text-white">
+        <h1>فروشگاه اینترنتی مقداد آی تی</h1>
+        <MdOutlineClose size={"20px"} className="cursor-pointer" />
+      </div>
+      <ul>
+        <li className="flex items-center justify-between p-4">
+          <Link
+            href="/products/laptops"
+            className="text-gray-800 text-sm hover:text-gray-950 hover:font-bold"
+          >
+            لپتاپ
+          </Link>
+          <RxCaretDown className="cursor-pointer" />
+        </li>
+        <li className="flex items-center justify-between  p-4">
+          <Link
+            href="/products/mobiles"
+            className="text-gray-800 text-sm hover:text-gray-950 hover:font-bold"
+          >
+            موبایل{" "}
+          </Link>
+          <RxCaretDown className="cursor-pointer" />
+        </li>
+        <li className="flex items-center justify-between p-4">
+          <Link
+            href="/products/tablets"
+            className="text-gray-800 text-sm hover:text-gray-950 hover:font-bold"
+          >
+            تبلت
+          </Link>
+          <RxCaretDown className="cursor-pointer" />
+        </li>
+        <li className="flex items-center justify-between  p-4">
+          <Link
+            href="/products/cameras"
+            className="text-gray-800 text-sm hover:text-gray-950 hover:font-bold"
+          >
+            دوربین
+          </Link>
+          <RxCaretDown className="cursor-pointer" />
+        </li>
+        <li className="flex items-center justify-between p-4">
+          <Link
+            href="/products/games"
+            className="text-gray-800 text-sm hover:text-gray-950 hover:font-bold"
+          >
+            کنسول و بازی
+          </Link>
+          <RxCaretDown className="cursor-pointer" />
+        </li>
+      </ul>
+    </aside>
+  );
 }
 
-export default OffCanvasMenu
+export default OffCanvasMenu;
